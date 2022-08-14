001ad907df6876899ac1286fd5c8d0d02860821d
Mathews <mathews.milimu@student.moringaschool.com>
2022-08-14 18:43:51 +0300

    Lab Done

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Your code here
+ const dodger = document.getElementById ("dodger");
+
+ const left = dodger.style.left;
+ const leftNumbers = dodger.style.left.replace("px","");
+ const leftpx = parseInt(leftNumbers, 10);
+
+ function moveDodgerLeft() {
+    dodger.style.left= `${leftpx - 10}px`;
+
+ }
+
+ function moveDodgerRight() {
+    dodger.style.left = `${leftpx + 10}px`;
+ }
